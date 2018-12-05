42d3266313bf314849fbd93d6f4e3352d7b0159e
tslight <tobyslight@gmail.com>
2018-12-05 00:29:25 +0000

    style and truncate_whitespace

--- intro.c
+++ intro.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void inout () {
+void echo_input() {
     int c;
     while ((c = getchar()) != EOF)
 	putchar(c);
 }
 
-void count_chars () {
+void count_chars() {
     double nc;
     printf("Enter a string: ");
     for (nc = 0; getchar() != '\n'; nc++);
     printf("Number of chars = %.0f\n", nc);
 }
 
-void count_lines () {
+void count_lines() {
     int c, nl;
     while ((c = getchar()) != EOF)
 	if (c == '\n')
@@ -21,7 +21,7 @@ void count_lines () {
     printf("Number of lines = %d\n", nl);
 }
 
-void count_words () {
+void count_words() {
     int c, nc, nw;
     nc = nw = 0;
     while ((c = getchar()))
@@ -37,9 +37,24 @@ void count_words () {
     printf("Number of words = %d\n", nw);
 }
 
-int main () {
-    /* inout(); */
+void truncate_whitespace() {
+    int c, s;
+    s = 0;
+    while ((c = getchar()) != EOF) {
+	if (c == ' ' || c == '\t'){
+	    ++s;
+	} else {
+	    s = 0;
+	}
+	if (s <= 1)
+	    putchar(c);
+    }
+}
+
+int main() {
+    /* echo_input (); */
     /* count_chars(); */
     /* count_lines(); */
-    count_words();
+    /* count_words(); */
+    truncate_whitespace();
 }
